07b3f56ac2a0b3ca2bba82f95036ad7a5389716a
ergz <erodriguez@flowwest.com>
2024-08-28 14:19:04 -0700

    debug

--- execute.sh
+++ execute.sh
@@ -10,8 +10,8 @@ set -e
 # capture cl arguments
 bid_name=$1
 auction_id=$2
-input_bucket=$3
-auction_shapefile=$4
+auction_shapefile=$3
+output_bucket=$4
 
 send_sqs_message() {
     local queue_url="$1"
@@ -31,10 +31,14 @@ send_sqs_message() {
         --message-attributes "$message_attributes"
 }
 
-
+echo "SET UP -----------------------------------"
+echo "bid name: $bid_name"
+echo "auction id: $auction_id"
+echo "auction shapefile: $auction_shapefile"
+echo "output bucket: $output_bucket"
 
 send_sqs_message "$QUEUE_URL" "<execute.sh> - Starting up bid run" "$bid_name"
 send_sqs_message "$QUEUE_URL" "<execute.sh> - Strating 02_analyze_bids.R" "$bid_name"
-Rscript --no-save scripts/02_analyze_bids.R $auction_id $auction_shapefile $input_bucket
+Rscript --no-save scripts/02_analyze_bids.R $auction_id $auction_shapefile $auction_id
 send_sqs_message "$QUEUE_URL" "<execute.sh> - Running 02_analyze_bids.R... DONE" "$bid_name"
 
